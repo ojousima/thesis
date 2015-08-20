c8e52d4c6d964111a9323aa6e00b1c76d635a4a5
Otso Jousimaa <otso@ojousima.net>
2015-08-20 11:39:35 +0300

    Single bias magnet

--- simulation/FEMM/force_cycle.lua
+++ simulation/FEMM/force_cycle.lua
@@ -8,28 +8,25 @@ for n=1,(200) do
 
 	mi_analyse(1)
 	mi_loadsolution()
-
+    
     if  n==1  then
 	  mo_resize(300,550)
 	  mo_zoomnatural()
 	end
+
+	mo_groupselectblock(2) -- Select rotor of output
 	mo_showdensityplot(0,0,0.5,0,"mag") 	   -- Obtain Image
-	alfa=dH*2*(n-1)
-	mo_clearblock()
-	mo_selectgroup(2)
-	--volume = mo_blockintegral(10)
-	force = mo_blockintegral(10) -- 19: steady-state weighted stress tensor
+	
+	force = mo_blockintegral(19) -- 19: steady-state weighted stress tensor
 	write(log, force)
 	write(log, ";")
-	mo_clearblock()
-	mo_selectgroup(2)
-	distance = (dH * n)
+	distance = (dH * n) -- calculate accumulated move
 	write(log, distance)
 	write(log, "\n")
-	mo_savebitmap(format("imgout/magnet_%1$d.bmp",alfa))  -- Save Image
-	mo_close()
+	mo_savebitmap(format("imgout/magnet_%1$d.bmp",n))  -- Save Image
+	--mo_close() keep result open to keep vlock selection and zoom
 
-	mi_selectgroup(2)			-- Select Rotor
+	mi_selectgroup(2)			-- Select Rotor f input
 	mi_movetranslate(0, dH)		-- move the rotor
 end
 
