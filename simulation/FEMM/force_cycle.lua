3ee5eb07ef3b44472e95c68d8dad1dd59e3390cd
Otso Jousimaa <otso@ojousima.net>
2015-08-24 15:35:21 +0300

    Add text on flux linkage

--- simulation/FEMM/force_cycle.lua
+++ simulation/FEMM/force_cycle.lua
@@ -1,6 +1,7 @@
 dH=0.1
 
 log = openfile("result.log", "w")
+write(log, "Distance;Force;Flux linkage")
 open("moving_magnet.fem")
 mi_saveas("temp.fem")
 
@@ -16,12 +17,18 @@ for n=1,(175) do
 
 	mo_groupselectblock(2) -- Select rotor of output
 	mo_showdensityplot(0,0,0.5,0,"mag") 	   -- Obtain Image
-	
+	distance = (dH * n) -- calculate accumulated move
+	write(log, distance)
+	write(log, ";")
 	force = mo_blockintegral(19) -- 19: steady-state weighted stress tensor
 	write(log, force)
 	write(log, ";")
-	distance = (dH * n) -- calculate accumulated move
-	write(log, distance)
+	mo_clearblock() -- clear previous selection
+	mo_groupselectblock(1) -- Select coil of output	
+	flux_integral = mo_blockintegral(9) -- 9: z-directional flux block integral
+	volume = mo_blockintegral(10) -- 10: Block volume
+	flux_linkage = flux_integral/volume -- Calculate flux linkage to coil
+	write(log, flux_linkage)
 	write(log, "\n")
 	mo_savebitmap(format("imgout/magnet_%1$d.bmp",n))  -- Save Image
 	--mo_close() keep result open to keep vlock selection and zoom
