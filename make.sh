b0204facc87d299c9f6823ea24da8392d3651a0e
Otso Jousimaa <otso@ojousima.net>
2015-10-15 10:56:58 +0300

    Automatically open pdf after compiling

--- make.sh
+++ make.sh
@@ -4,4 +4,4 @@ pdflatex -interaction=nonstopmode thesis.tex > 1st.log
 bibtex thesis > 2nd.log
 pdflatex -interaction=nonstopmode thesis.tex > 3rd.log
 pdflatex -interaction=nonstopmode thesis.tex > 4th.log
-
+epdfview thesis.pdf
